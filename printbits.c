25161f50db8778f2e85c5685030d735392ae94d8
Kare Nuorteva <kare.nuorteva@me.com>
2017-02-02 23:02:41 +0200

    Rewrite in Plan 9 C

--- printbits.c
+++ printbits.c
@@ -1,21 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <u.h>
+#include <libc.h>
 
-void to_bit_string(short bits, char *result) {
-	for (int i = 7; i > -1; i--) {
-		*result++ = (bits & 1) + '0';
-		bits >>= 1;
+void main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprint(2, "usage: %s integer\n", argv[0]);
+		exits("usage");
 	}
-}
-
-int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		fprintf(stderr, "Usage: printbits integer\n");
-		exit(EXIT_FAILURE);
-	}
-	char result[9];
-	memset(result, '\0', 8);
-	to_bit_string(atoi(argv[1]), result);
-	printf("%s\n", result);
+	int bits = atoi(argv[1]);
+	print("%b\n", bits);
+	exits(0);
 }
